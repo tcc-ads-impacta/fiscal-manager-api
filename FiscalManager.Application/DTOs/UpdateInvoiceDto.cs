3707fc7af773c07c2a5643c0b601dbc5e15e1f57
Naassom Pedro <naassom7@gmail.com>
2026-04-03 16:20:24 -0300

    fix: ajusta dto de atualização de registro

--- FiscalManager.Application/DTOs/UpdateInvoiceDto.cs
+++ FiscalManager.Application/DTOs/UpdateInvoiceDto.cs
@@ -2,18 +2,18 @@
 
 namespace FiscalManager.Application.DTOs;
 
-//public record UpdateInvoiceDto
-//{
-//    public string Description { get; init; } = string.Empty;
-//    public decimal Amount { get; init; }
-//    public DateTime Date { get; init; }
-//    public IFormFile? File { get; init; }
-//}
-
 public record UpdateInvoiceDto
-(
-    string Description,
-    decimal Amount,
-    DateTime Date,
-    IFormFile? File
-);
+{
+    public string Description { get; init; } = string.Empty;
+    public decimal Amount { get; init; }
+    public DateTime Date { get; init; }
+    public IFormFile? File { get; init; }
+}
+
+//public record UpdateInvoiceDto
+//(
+//    string Description,
+//    decimal Amount,
+//    DateTime Date,
+//    IFormFile? File = null
+//);
